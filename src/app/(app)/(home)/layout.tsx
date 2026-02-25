0a7e7b87d14920e82c7b69f8ed7b6efe1f86acf5
dokhai130203 <dohuynhkhai130203@gmail.com>
2026-02-25 23:25:42 +0700

    implement navbar and sidebar with nested navigation

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -5,6 +5,7 @@ import { getPayload } from 'payload'
 import { NavBar } from "./navbar";
 import { Footer } from "./footer";
 import { SearchFilters } from "./search-filters";
+import { CustomCategory } from './type';
 
 interface Props {
     children: React.ReactNode;
@@ -24,21 +25,18 @@ const Layout = async ({ children }: Props) => {
         exists: false,
       },
     },
+    sort: "name",
   });
 
-  const formattedData = data.docs.map((doc) => ({
+  const formattedData: CustomCategory[] = data.docs.map((doc) => ({
     ...doc,
     subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
       // Because of "depth: 1", these are fully populated Category objects
       // Because of "depth: 1", we are confident "doc" will be a type of "Category"
       ...(doc as Category),
+      subcategories: undefined,
     }))
   }));
-
-  console.log({
-    data, 
-    formattedData
-  });
 
     return (
         <div className="flex flex-col min-h-screen">
