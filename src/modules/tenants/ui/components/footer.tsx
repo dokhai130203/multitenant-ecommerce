b97e80f4554d753683ac5a6ffa3f4bda295a63a4
dokhai130203 <dohuynhkhai130203@gmail.com>
2026-06-05 16:59:26 +0700

    add cover field, formatCurrency util, fix product card links

--- src/modules/tenants/ui/components/footer.tsx
+++ src/modules/tenants/ui/components/footer.tsx
@@ -12,7 +12,7 @@ export const Footer = () => {
     return (
         <footer className="border-t font-medium bg-white">
             <div className="max-w-(--breakpoint-xl) mx-auto flex items-center h-full gap-2 px-4 py-6 lg:px-12">
-                <p>Power by</p>
+                <p>Powered by</p>
                 <Link href="/">
                     <span className={cn("text-2xl font-semibold", poppins.className)}>
                         funroad
