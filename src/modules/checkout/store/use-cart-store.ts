1833e463b879f1dab93b49421698387ca359c683
dokhai130203 <dohuynhkhai130203@gmail.com>
2026-06-08 18:03:40 +0700

    add per-tenant shopping cart with zustand and cart/checkout buttons

---
+++ src/modules/checkout/store/use-cart-store.ts
@@ -0,0 +1,65 @@
+import { create } from 'zustand';
+import { createJSONStorage, persist } from 'zustand/middleware';
+
+interface TenantCart {
+    productIds: string[]; // Tenant Cart └── list of product IDs
+};
+
+interface CartState { // State = data in the wardrobe
+    tenantCarts: Record<string, TenantCart>; //key: [tenantSlug]: value TenantCart
+    addProduct: (tenantSlug: string, productId: string) => void; // action
+    removeProduct: (tenantSlug: string, productId: string) => void;
+    clearCart: (tenantSlug: string) => void;
+    clearAllCarts: () => void;
+    getCartByTenant: (tenantSlug: string) => string[]; // selector function to get the cart for a specific tenant
+};
+
+export const useCartStore = create<CartState>()( // Store = A common wardrobe for the entire app
+    persist( // remember after refresh
+       (set, get) => ({ // // set and get are functions to update and access the state
+            tenantCarts: {}, // initial state
+            addProduct: (tenantSlug, productId) => 
+                set((state) => ({
+                    tenantCarts: {
+                        ...state.tenantCarts, // keep existing carts
+                        [tenantSlug]: {
+                            productIds: [ // create a new cart for the tenant
+                                ...(state.tenantCarts[tenantSlug]?.productIds || []), // if tenant cart exists, keep existing productIds, otherwise start with an empty array
+                                productId, // immutable update
+                            ]
+                        },
+                    },
+                })),
+            removeProduct: (tenantSlug, productId) => 
+                set((state) => ({
+                    tenantCarts: {
+                        ...state.tenantCarts,
+                        [tenantSlug]: {
+                            productIds: state.tenantCarts[tenantSlug]?.productIds.filter(
+                                (id) => id !== productId
+                            ) || [],
+                        },
+                    },
+                })),
+            clearCart: (tenantSlug) => 
+                set((state) => ({
+                    tenantCarts: {
+                        ...state.tenantCarts,
+                        [tenantSlug]: {
+                            productIds: []
+                        },
+                    },
+                })),
+            clearAllCarts: () =>
+                set({
+                    tenantCarts: {},
+                }),
+            getCartByTenant: (tenantSlug) =>
+                get().tenantCarts[tenantSlug]?.productIds || [], 
+        }),
+       {
+        name: "funroad-cart",
+        storage: createJSONStorage(() => localStorage), // use localStorage to persist the cart
+       },
+    ),
+);
