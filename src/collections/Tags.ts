fdf7d0b13dda126ab79bfb2cfef8e3f2f729689f
dokhai130203 <dohuynhkhai130203@gmail.com>
2026-06-14 15:10:33 +0700

    restrict CMS collections with role-based and tenant-aware permissions

--- src/collections/Tags.ts
+++ src/collections/Tags.ts
@@ -1,9 +1,18 @@
 import type { CollectionConfig } from "payload"
 
+import { isSuperAdmin } from "@/lib/access";
+
 export const Tags: CollectionConfig = {
   slug: 'tags',
+  access: {
+    read: () => true,
+    create: ({ req }) => isSuperAdmin(req.user),
+    update: ({ req }) => isSuperAdmin(req.user),
+    delete: ({ req }) => isSuperAdmin(req.user),
+  },
   admin: {
     useAsTitle: 'name',
+    hidden: ({ user }) => !isSuperAdmin(user), // only super-admins can see the Tags collection in the admin
   },
   fields: [
     {
--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -1,26 +1,39 @@
 import type { CollectionConfig } from "payload"
 import { tenantsArrayField } from "@payloadcms/plugin-multi-tenant/fields";
 
+import { isSuperAdmin } from "@/lib/access";
+
 const defaultTenantArrayField = tenantsArrayField({
   tenantsArrayFieldName: "tenants", // the field on the user is called tenants // user.tenants
   tenantsCollectionSlug: "tenants", // it points to your Tenants collection
   tenantsArrayTenantFieldName: "tenant", // inside each array item, the relationship field is called tenant
   arrayFieldAccess: {
     read: () => true,
-    create: () => true,
-    update: () => true,
+    create: ({ req }) => isSuperAdmin(req.user),
+    update: ({ req }) => isSuperAdmin(req.user),
   },
   tenantFieldAccess: {
     read: () => true,
-    create: () => true,
-    update: () => true,
+    create: ({ req }) => isSuperAdmin(req.user),
+    update: ({ req }) => isSuperAdmin(req.user),
   },
 });
 
 export const Users: CollectionConfig = {
   slug: 'users',
+  access: {
+    read: () => true,
+    create: ({ req }) => isSuperAdmin(req.user),
+    delete: ({ req }) => isSuperAdmin(req.user),
+    update: ({ req, id }) => {
+      if(isSuperAdmin(req.user)) return true;
+
+      return req.user?.id === id; // users can update themselves, but not other users
+    }
+  },
   admin: {
     useAsTitle: 'email',
+    hidden: ({ user }) => !isSuperAdmin(user), // only super-admins can see the Users collection in the admin
   },
   auth: true,
   fields: [
@@ -39,6 +52,9 @@ export const Users: CollectionConfig = {
       defaultValue: ["user"],
       hasMany: true,
       options: ["super-admin", "user"],
+      access: {
+        update: ({ req }) => isSuperAdmin(req.user), // only super-admins can update roles
+      }
     },
     {
       ...defaultTenantArrayField,
